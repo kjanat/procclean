279d8bdde6817f6ce127ffcf0aadeff40439c1b4
Kaj Kowalski <info@kajkowalski.nl>
2025-11-27 19:50:56 +0100

    Add dev dependencies: ruff, pytest, pre-commit, ty

--- src/procclean/_version.py
+++ src/procclean/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.1.dev3+gb8e0d10e7.d20251127'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev3', 'gb8e0d10e7.d20251127')
+__version__ = version = '0.1.1.dev5+gfcceedf4c.d20251127'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev5', 'gfcceedf4c.d20251127')
 
 __commit_id__ = commit_id = None
